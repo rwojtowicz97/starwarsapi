7fa01722f9bd208ac4bb3a9a05e8403dc9c2dcff
Robert <43409796+JuniorScaper@users.noreply.github.com>
2019-03-07 23:42:07 +0100

    Update Mounting in App.js

--- src/Components/People/People.js
+++ src/Components/People/People.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const People = ({ name, height, gender}) => {
 	return (
-		<div className="">
+	
 			<div>
 					<h2>{name}</h2>
 					<p>Height: {height}</p>
 					<p>Gender: {gender}</p>
 			</div>
-		</div>
+		
 		);
 }
 
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 import PlanetList from '../Components/Planet/PlanetList.js'
+import PeopleList from '../Components/People/PeopleList.js'
+
 
 class App extends Component {
   constructor() {
     super()
     this.state = { 
-      planets: []
+      planets: [],
+      people: []
     }
   }
 
   componentDidMount() {
-    fetch('https://swapi.co/api/planets/')
-    .then(response => response.json())
-    .then(planety => this.setState ({planets: planety.results}))
+    // fetch('https://swapi.co/api/planets/')
+    // .then(response => response.json())
+    // .then(data => this.setState ({planets: data.results}));
+
+    // fetch('https://swapi.co/api/people/')
+    // .then(response1 => response1.json())
+    // .then(data1 => this.setState ({people: data1.results}));
+    Promise.all([
+        fetch('https://swapi.co/api/planets/'),
+        fetch('https://swapi.co/api/people/')
+      ])
+      .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
+      .then(([data1, data2]) => this.setState({
+        planets: data1.results,
+        //people: data2.results
+      }))
+
   }
 
  
   render() {
-    const { planets } = this.state;
+    const { planets, people } = this.state;
     return (
       <div>
-        <h1>Star Wars</h1>
           <div className="container">
            <div className="zone planets"><PlanetList planets={planets} /></div>
+           <div className="zone people"><PeopleList people={people} /></div>
+          
           </div>
       </div>
 
